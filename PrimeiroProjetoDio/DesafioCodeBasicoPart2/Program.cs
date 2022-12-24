754bdd033a216d77a16330d32c65eb9bb9953200
Isaac Bastos <isaacbilac920@gmail.com>
2022-12-24 04:57:44 -0300

    last challenge complete

--- PrimeiroProjetoDio/DesafioCodeBasicoPart2/Program.cs
+++ PrimeiroProjetoDio/DesafioCodeBasicoPart2/Program.cs
@@ -1,25 +1,27 @@
 ﻿using System.IO;
 using System.Linq;
+using System;
 
 
-int quantidadeDeEntradas = int.Parse(Console.ReadLine());
-
-string[] entrada = new string[2];
-
-for (int contador = 0; contador < quantidadeDeEntradas; contador++)
+class Program
 {
-    entrada = Console.ReadLine().Split(" ");
+    static void Main(string[] args)
+    {
+        int quantidadeDeEntradas = int.Parse(Console.ReadLine());
+        string[] entrada = new string[2];
 
-    string primeiraEntrada = entrada[0];
-    string segundaEntrada = entrada[1];
+        for (int i = 0; i < quantidadeDeEntradas; ++i)
+        {
+            entrada = Console.ReadLine().Split(" ");
 
-    if (segundaEntrada.Length > primeiraEntrada.Length)
-        Console.WriteLine("nao encaixa");
-
-    else if (primeiraEntrada.EndsWith(segundaEntrada))
-        Console.WriteLine("encaixa");
-
-    else
-        Console.WriteLine("nao encaixa");
-
+            string primeiraEntrada = entrada[0];
+            string segundaEntrada = entrada[1];
+            if (segundaEntrada.Length > primeiraEntrada.Length)
+                Console.WriteLine("nao encaixa");
+            else if (primeiraEntrada.EndsWith(segundaEntrada))
+                Console.WriteLine("encaixa");
+            else
+                Console.WriteLine("nao encaixa");
+        }
+    }
 }
